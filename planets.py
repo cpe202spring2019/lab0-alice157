093e3ea8dd60934db176654014523f371f41d735
Sam Burns <samburns157@gmail.com>
2019-04-03 14:29:03 -0700

    Finished Lab0, added .gitignore

--- planets.py
+++ planets.py
@@ -1,7 +1,9 @@
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    output_string = "\nOn Mars you would weigh {} pounds.\nOn Jupiter you would weigh {} pounds."
+    earth_weight = int(input("What do you weigh on earth? "))
+    mars_weight = earth_weight * 0.38
+    jupiter_weight = earth_weight * 2.34
+    print(output_string.format(mars_weight, jupiter_weight))
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
